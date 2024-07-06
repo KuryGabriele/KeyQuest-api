d43e17a4f6de3152a7c17fd814fcbd7b57f408b7
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-07-06 12:02:51 +0200

    Updated query to account for users having a score

--- routes/levels.js
+++ routes/levels.js
@@ -10,24 +10,24 @@ router.get("/", fullAuthenticationMiddleware, (req, res) => {
     const id = req.body.id;
     if (!id) return res.status(400).send({ message: "Missing id parameter." });
 
-    req.database.query("SELECT * FROM users_scores as u, levels as l WHERE l.id=u.lId && u.uId=?", [id], (err, result, fields) => {
+    req.database.query("SELECT l.id, l.displayName, l.difficulty, l.notes, COALESCE(us.score, 0) AS score FROM levels l LEFT JOIN users_scores us ON l.id = us.lId AND us.uId = 1", [id], (err, result, fields) => {
         if (err) console.error(err);
-        if (err) return res.status(500).send({ message: "Something went wrong." });
+    if (err) return res.status(500).send({ message: "Something went wrong." });
 
-        const plate = result[0];
-        var jsonOut = [];
-        result.forEach((plate) => {
-            jsonOut.push({
-                id: plate.id,
-                displayName: plate.displayName,
-                difficulty: plate.difficulty,
-                bestScore: plate.score,
-                notes: plate.notes
-            });
+    const plate = result[0];
+    var jsonOut = [];
+    result.forEach((plate) => {
+        jsonOut.push({
+            id: plate.id,
+            displayName: plate.displayName,
+            difficulty: plate.difficulty,
+            bestScore: plate.score || 0,
+            notes: plate.notes
         });
-        
-        return res.status(200).json(jsonOut);
     });
+
+    return res.status(200).json(jsonOut);
+});
 });
 
 module.exports = router;
